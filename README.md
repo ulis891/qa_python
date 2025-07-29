92cdf10f155ca26fc3b803c18ab95af7aefee535
Aleksandr Kostromin <ulis89@yandex.ru>
2025-07-29 20:34:19 +0300

    add sprint4 task

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # qa_python
+## Описание фикстур:
+- collector: создаёт экземпляр класса BooksCollector
+- collector_with_books: создаёт экземпляр класса BooksCollector с книгами
+- collector_with_books_and_genre: создаёт экземпляр класса BooksCollector с книгами и жанрамии 
+## Описание тестов
+- **test_add_new_book_one_book**: проверяет добавление новой книги.
+- **test_add_new_book_wrong_len_book_name**: проверяет, что метод add_new_book не добавляет книгу с названием, которое не соответствует требованию по длинне названия
+- **test_add_new_book_right_len_book_name**: проверяет, что метод add_new_book добавляет книгу с названием, которое соответствует требованию по длинне названия.
+- **test_set_book_genre_right_genre**: проверяет, что метод set_book_genre устанавливает жанр для книги если жанр существует.
+- **test_get_book_genre**: проверяет, что метод get_books_genre возвращает жанр книги.
+- **test_get_books_with_specific_genre_in_dictionary**: проверяет, что метод get_books_with_specific_genre возвращает список книг с указанным жанром
+- **test_get_books_with_specific_genre_missing_from_dictionary**: проверяет, что метод get_books_with_specific_genre возвращает пустой список, если книги с указанным жанром нет в коллекции
+- **test_get_books_with_specific_genre_books_wrong_genre**: проверяет, что метод get_books_with_specific_genre возвращает пустой список, если жанр не существует в списке жанров
+- **test_get_books_genre_empty_dict**: проверяет, что метод get_books_genre возвращает пустой словарь, если нет книг в коллекции
+- **test_get_books_genre_not_empty_dict**: проверяет, что метод get_books_genre возвращает словарь, если есть книги в коллекции.
+- **test_get_books_for_children_adult_books**: проверяет, что метод get_books_for_children возвращает пустой список книг для детей, если в коллекции только взрослые книги.
+- **test_get_books_for_children_books_for_children**: проверяет, что метод get_books_for_children возвращает список книг для детей, если в коллекции есть детские книги.
+- **test_get_books_for_children_books_mix_books**: проверяет, что метод get_books_for_children возвращает список книг где нет книг для взрослых, если в коллекции есть детские и взрослые книги.
+- **test_add_book_in_favorites_book_in_dict**: проверяет, что метод add_book_in_favorites добавляет книгу в избранное, если книга есть в коллекции.
+- **test_add_book_in_favorites_book_not_in_dict**:  проверяет, что метод add_book_in_favorites не добавляет книгу в избранное, если книги нет в коллекции.
+- **test_add_book_in_favorites_book_in_favorites_two_identical_books**: Тест проверяет, что метод add_book_in_favorites не добавляет книгу в избранное, если она уже есть в избранном.
+- **test_delete_book_from_favorites**: проверяет, что метод delete_book_from_favorites удаляет книгу из избранного.
+
+## Тестовое покрытие
+```
+Name      Stmts   Miss  Cover
+-----------------------------
+main.py      38      0   100%
+-----------------------------
+TOTAL        38      0   100%
+
+```
--- tests.py
+++ tests.py
@@ -1,24 +1,228 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.fixture
+    def collector(self):
+        """
+        Создает экземпляр класса BooksCollector
+        :return: BooksCollector() - экземпляр класса
+        """
+        return BooksCollector()
+
+    @pytest.fixture
+    def collector_with_books(self):
         collector = BooksCollector()
+        books = ['Гарри Поттер',
+                 'Шерлок Холмс',
+                 'Винни Пух',
+                 'Что делать, ваш кот хочет вас убить',
+                 'Гордость и предубеждение и зомби'
+                 ]
+        for book in books:
+            collector.add_new_book(book)
+        return collector
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    @pytest.fixture
+    def collector_with_books_and_genre(self):
+        collector = BooksCollector()
+        books = {'Гарри Поттер': 'Фантастика',
+                 'Шерлок Холмс': 'Детективы',
+                 'Винни Пух': 'Мультфильмы',
+                 'Что делать, ваш кот хочет вас убить': 'Ужасы',
+                 'Гордость и предубеждение и зомби': 'Комедии'
+                 }
+        for book, genre in books.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+        return collector
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_one_book(self, collector):
+        """
+        Тест проверяет, что метод add_new_book добавляет книгу и у добавленной книги нет жанра
+        """
+        name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(name)
+        assert name in collector.books_genre and collector.books_genre[name] == ''
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize("name", [
+        '',
+        'Название книги длинной больше 40 символов'])
+    def test_add_new_book_wrong_len_book_name(self, collector, name):
+        """
+        Тест проверяет, что метод add_new_book не добавляет книгу с названием, которое не соответствует требованиям
+        :param collector: созданный экземпляр класса BooksCollector
+        :param name: Название книги
+        """
+        collector.add_new_book(name)
+        assert name not in collector.books_genre
+
+    @pytest.mark.parametrize("name", [
+        '1',
+        'Нормальное название',
+        'Название книги из сорока символов ровно!'])
+    def test_add_new_book_right_len_book_name(self, collector, name):
+        """
+        Тест проверяет, что метод add_new_book добавляет книгу с названием, которое соответствует требованиям
+        :param collector: созданный экземпляр класса BooksCollector
+        :param name: Название книги
+        """
+        collector.add_new_book(name)
+        assert name in collector.books_genre
+
+    @pytest.mark.parametrize("name, genre, expected_genre", [
+        ('Гарри Поттер', 'Фантастика', 'Фантастика'),
+        ('Шерлок Холмс', 'Детективы', 'Детективы'),
+        ('Винни Пух', 'Мультфильмы', 'Мультфильмы'),
+        ('Что делать, если ваш кот хочет вас убить', 'Ужасы', 'Ужасы'),
+        ('Гордость и предубеждение и зомби', 'Торт', '')])
+    def test_set_book_genre_right_genre(self, collector, name, genre, expected_genre):
+        """
+        Тест проверяет, что метод set_book_genre устанавливает жанр для книги если жанр существует.
+        :param collector: созданный экземпляр класса BooksCollector
+        :param name: название книги
+        :param genre: жанр
+        :param expected_genre: ожидаемый жанр
+        """
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_book_genre(name) == expected_genre
+
+    def test_get_book_genre(self, collector):
+        """
+        Тест проверяет, что метод get_books_genre возвращает жанр книги.
+        :param collector: созданный экземпляр класса BooksCollector
+        """
+        name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, 'Комедии')
+        assert collector.get_book_genre(name) == 'Комедии'
+
+    @pytest.mark.parametrize("name, genre", [
+        ('Гарри Поттер', 'Фантастика'),
+        ('Шерлок Холмс', 'Детективы'),
+        ('Винни Пух', 'Мультфильмы'),
+        ('Что делать, ваш кот хочет вас убить', 'Ужасы'),
+        ('Гордость и предубеждение и зомби', 'Комедии')])
+    def test_get_books_with_specific_genre_in_dictionary(self, collector_with_books_and_genre, name, genre):
+        """
+        Тест проверяет, что метод get_books_with_specific_genre возвращает список книг с указанным жанром,
+        если жанр есть в словаре.
+        :param collector_with_books_and_genre: созданный экземпляр класса BooksCollector с книгами и жанрами
+        """
+        assert collector_with_books_and_genre.get_books_with_specific_genre(genre) == [name]
+
+    @pytest.mark.parametrize("genre", ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_get_books_with_specific_genre_missing_from_dictionary(self, collector_with_books, genre):
+        """
+        Тест проверяет, что метод get_books_with_specific_genre возвращает пустой список,
+        если книги с указанным жанром нет в коллекции.
+        :param collector_with_books - экземпляр класса BooksCollector с книгами
+        """
+        assert collector_with_books.get_books_with_specific_genre(genre) == []
+
+    def test_get_books_with_specific_genre_books_wrong_genre(self, collector_with_books_and_genre):
+        """
+        Тест проверяет, что метод get_books_with_specific_genre возвращает пустой список,
+        если жанр не существует в списке жанров.
+        :param collector_with_books_and_genre - экземпляр класса BooksCollector с книгами и жанрами
+        """
+        assert collector_with_books_and_genre.get_books_with_specific_genre('иидемоК') == []
+
+    def test_get_books_genre_empty_dict(self, collector):
+        """
+        Тест проверяет, что метод get_books_genre возвращает пустой словарь, если нет книг в коллекции.
+        :param collector:
+        """
+        assert collector.get_books_genre() == {}
+
+    @pytest.mark.parametrize("name, genre", [
+        ('Гарри Поттер', 'Фантастика'),
+        ('Шерлок Холмс', 'Детективы'),
+        ('Винни Пух', 'Мультфильмы'),
+        ('Что делать, ваш кот хочет вас убить', 'Ужасы'),
+        ('Гордость и предубеждение и зомби', 'Комедии')])
+    def test_get_books_genre_not_empty_dict(self, collector_with_books_and_genre, name, genre):
+        """
+        Тест проверяет, что метод get_books_genre возвращает словарь, если есть книги в коллекции.
+         и словарь соответствует добавленным книгам.
+        :param collector_with_books_and_genre - экземпляр класса BooksCollector с книгами и жанрами
+        """
+        assert collector_with_books_and_genre.get_books_genre()[name] == genre
+
+    def test_get_books_for_children_adult_books(self, collector):
+        """
+        Тест проверяет, что метод get_books_for_children возвращает пустой список книг для детей,
+        если в коллекции только взрослые книги.
+        :param collector:
+        """
+        collector.add_new_book('Гадость и предрассудки и зомби')
+        collector.set_book_genre('Гадость и предрассудки и зомби', 'Ужасы')
+        collector.add_new_book('Шерлок Холмс')
+        collector.set_book_genre('Шерлок Холмс', 'Детективы')
+        assert collector.get_books_for_children() == []
+
+    def test_get_books_for_children_books_for_children(self, collector):
+        """
+        Тест проверяет, что метод get_books_for_children возвращает список книг для детей,
+        если в коллекции есть детские книги.
+        :param collector:
+        """
+        collector.add_new_book('Винни Пух')
+        collector.set_book_genre('Винни Пух', 'Мультфильмы')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        assert collector.get_books_for_children() == ['Винни Пух', 'Гарри Поттер']
+
+    def test_get_books_for_children_books_mix_books(self, collector_with_books_and_genre):
+        """
+        Тест проверяет, что метод get_books_for_children возвращает список книг где нет книг для взрослых,
+        если в коллекции есть детские и взрослые книги.
+        :param collector_with_books_and_genre - экземпляр класса BooksCollector с книгами и жанрами
+        """
+        adult_books = ['Шерлок Холмс', 'Что делать, ваш кот хочет вас убить']
+        for book in adult_books:
+            assert book not in collector_with_books_and_genre.get_books_for_children()
+
+    def test_add_book_in_favorites_book_in_dict(self, collector):
+        """
+        Тест проверяет, что метод add_book_in_favorites добавляет книгу в избранное,
+        если книга есть в коллекции.
+        :param collector:
+        """
+        name = 'Гарри Поттер'
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        assert name in collector.favorites
+
+    def test_add_book_in_favorites_book_not_in_dict(self, collector):
+        """
+        Тест проверяет, что метод add_book_in_favorites не добавляет книгу в избранное,
+        если книги нет в коллекции.
+        :param collector:
+        """
+        collector.add_book_in_favorites('Гарри Поттер')
+        assert collector.favorites == []
+
+    def test_add_book_in_favorites_book_in_favorites_two_identical_books(self, collector_with_books):
+        """
+        Тест проверяет, что метод add_book_in_favorites не добавляет книгу в избранное,
+        если она уже есть в избранном.
+        :param collector_with_books - экземпляр класса BooksCollector с книгами
+        """
+        name = 'Гарри Поттер'
+        collector_with_books.add_book_in_favorites(name)
+        collector_with_books.add_book_in_favorites(name)
+        assert name in collector_with_books.favorites and len(collector_with_books.favorites) == 1
+
+    def test_delete_book_from_favorites(self, collector_with_books):
+        """
+        Тест проверяет, что метод delete_book_from_favorites удаляет книгу из избранного.
+        :param collector:
+        """
+        collector_with_books.add_book_in_favorites('Гарри Поттер')
+        collector_with_books.add_book_in_favorites('Шерлок Холмс')
+        collector_with_books.delete_book_from_favorites('Гарри Поттер')
+        assert collector_with_books.get_list_of_favorites_books() == ['Шерлок Холмс']
